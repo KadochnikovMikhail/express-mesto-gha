2a8a7d8e24135265acb33fc23930215a4106e274
Kadochnikov Mikhail <kadochnikov.mihaylo@yandex.ru>
2022-04-28 00:01:28 +0500

    13 REFACT

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
+
+
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const isEmail = require('validator/lib/isEmail');
-const BadAuthError = require('../errors/bad-auth-err');
-const { AVATAR_REGEX } = require('../constants');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -20,41 +18,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    validate: {
-      validator: (v) => AVATAR_REGEX.test(v),
-      message: 'Некорректная ссылка',
-    },
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (value) => isEmail(value),
-      message: 'Некорректный email',
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-});
-
-userSchema.statics.findUserByCredentials = function findUser(email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new BadAuthError('Неправильные почта или пароль'));
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new BadAuthError('Неправильные почта или пароль'));
-          }
-          return user;
-        });
-    });
-};
+  }
+})
 
 module.exports = mongoose.model('user', userSchema);
