dc13ee454d0e275ce3fc5940d089f654502e04fc
Kadochnikov Mikhail <kadochnikov.mihaylo@yandex.ru>
2022-05-04 10:06:00 +0500

    13proj12

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,8 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send({data: users}))
-    .catch((err) => res.status(500).send({data: err.message}));
+    .then((users) => res.status(200).send({ data: users }))
+    .catch((err) => res.status(500).send({ data: err.message }));
 };
 
 module.exports.getUserById = (req, res) => {
@@ -12,75 +12,75 @@ module.exports.getUserById = (req, res) => {
     User.findById(req.params.userId)
       .then((user) => {
         if (user == null) {
-          res.status(404).send({message: 'Пользователь с данным Id не найден'});
+          res.status(404).send({ message: 'Пользователь с данным Id не найден' });
         } else {
-          res.status(200).send({data: user});
+          res.status(200).send({ data: user });
         }
       })
       .catch((err) => {
-        res.status(500).send({data: err.message});
+        res.status(500).send({ data: err.message });
       });
   } else {
-    res.status(400).send({message: 'Введен некорректный id'});
+    res.status(400).send({ message: 'Введен некорректный id' });
   }
 };
 
 module.exports.createUser = (req, res) => {
-  const {name, about, avatar} = req.body;
+  const { name, about, avatar } = req.body;
 
-  User.create({name, about, avatar})
+  User.create({ name, about, avatar })
     .then((user) => {
-      res.status(200).send({data: user});
+      res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({message: `Возникла ошибка ${err.message}`});
+        res.status(400).send({ message: `Возникла ошибка ${err.message}` });
       } else {
-        res.status(500).send({message: `Возникла ошибка ${err.message}`});
+        res.status(500).send({ message: `Возникла ошибка ${err.message}` });
       }
-      res.send({data: err.message});
+      res.send({ data: err.message });
     });
 };
 
 module.exports.updateUser = (req, res) => {
-  const {name, about} = req.body;
+  const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, {name, about}, {new: true, runValidators: true})
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
 
     .then((user) => {
       if (!user) {
-        res.status(404).send({data: 'Пользователь с данным Id не найден'});
+        res.status(404).send({ data: 'Пользователь с данным Id не найден' });
       } else {
-        res.status(200).send({user});
+        res.status(200).send({ user });
       }
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({message: `Возникла ошибка ${err.message}`});
+        res.status(400).send({ message: `Возникла ошибка ${err.message}` });
       } else {
-        res.status(500).send({message: `Возникла ошибка ${err.message}`});
+        res.status(500).send({ message: `Возникла ошибка ${err.message}` });
       }
-      res.send({data: err.message});
+      res.send({ data: err.message });
     });
 };
 
 module.exports.updateUserAvatar = (req, res) => {
-  const {avatar} = req.body;
+  const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, {avatar}, {new: true, runValidators: true, upsert: true})
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true, upsert: true })
     .then((user) => {
       if (!user) {
-        res.status(404).send({data: 'Пользователь с данным Id не найден'});
+        res.status(404).send({ data: 'Пользователь с данным Id не найден' });
       } else {
-        res.status(200).send({user});
+        res.status(200).send({ user });
       }
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({message: `Возникла ошибка ${err.message}`});
+        res.status(400).send({ message: `Возникла ошибка ${err.message}` });
       } else {
-        res.status(500).send({message: `Возникла ошибка ${err.message}`});
+        res.status(500).send({ message: `Возникла ошибка ${err.message}` });
       }
-      res.send({data: err.message});
+      res.send({ data: err.message });
     });
 };
