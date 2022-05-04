454d3c69912f39a5b4fc2c16eaddec34d1ddab46
Kadochnikov Mikhail <kadochnikov.mihaylo@yandex.ru>
2022-05-05 00:29:58 +0500

    13proj23

--- controllers/cards.js
+++ controllers/cards.js
@@ -27,7 +27,7 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => { res.status(200).send({ data: card }); })
-    .catch((err) => () => {
+    .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: `Возникла ошибка ${err.message}` });
       } else {
